5a862ccdc8569e4e68584aa0dca1ab4cd38b8b6e
bebo698 <attasalah34@gmail.com>
2022-09-24 15:39:17 +0200

    Update config

--- js/App.js
+++ js/App.js
@@ -9,6 +9,8 @@ let total = document.getElementById('total');
 let count = document.getElementById('count');
 let submit = document.getElementById('submit');
 let category = document.getElementById('category');
+let mood = 'create';
+let temp ;
 
 function gettotal() {
 
@@ -109,7 +111,30 @@ submit.onclick = function () {
         //     }
 
         // }
-        datapro.push(newpro);
+        if (mood === 'create' )
+        {
+            if (newpro.count > 1 )
+            {
+                for (let i=0 ; i<newpro.count ; i++)
+                {
+                    datapro.push(newpro);
+    
+                }
+    
+             } else {
+                    datapro.push(newpro);
+    
+                }
+        }
+        else {
+            datapro[temp]= newpro;
+            mood = 'create';
+            submit.innerHTML='Create';
+            count.style.display = 'block';
+
+        }
+      
+        
         console.log(datapro);
         localStorage.setItem('product', JSON.stringify(datapro));
         ShowData();
@@ -135,15 +160,17 @@ function ShowData() {
        <td>${datapro[i].total}</td>
        <td>${datapro[i].category}</td>
 
-       <td><button id="update">Update</button></td>
+       <td><button onclick="update(${i})" id="update">Update</button></td>
        <td><button onclick="DeleteData(${i})" id="delete">delete</button></td>
    </tr>`
     }
     document.getElementById('tbody').innerHTML = table;
     let btnDelete = document.getElementById('deleteAll');
     if (datapro.length > 0) {
-        btnDelete.innerHTML = `<button onclick="DeleteAll()" id="deleteall">DeleteAll</button>`
+        btnDelete.innerHTML = `<button onclick="DeleteAll()" id="deleteall">DeleteAll ${datapro.length} </button>`
     }
+
+  
 
 }
 ShowData()
@@ -189,7 +216,27 @@ function DeleteData(i) {
 }
 
 
-//update
+//updata 
+function  update (i)
+{
+    title.value = datapro[i].title;
+    price.value = datapro[i].price;
+    taxes.value = datapro[i].taxes;
+    ads.value = datapro[i].ads;
+    category.value = datapro[i].category;
+    title.value = datapro[i].title;
+    discount.value = datapro[i].discount;
+    count.value = datapro[i].count;
+    count.style.display='none';
+    gettotal ();
+    submit.innerHTML= 'UPDATE'
+    mood = 'update';
+    temp=i;
+    // datapro[i].push ();
+
+
+ } 
+
 
 // search
 
